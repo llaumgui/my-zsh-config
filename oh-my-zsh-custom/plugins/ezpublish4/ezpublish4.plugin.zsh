c14b072b6fb96253e5fce6c855dfce41c3fe4caf
Guillaume Kulakowski <guillaume@kulakowski.fr>
2014-08-26 13:43:12 +0200

    Add some eZ4 aliases

--- oh-my-zsh-custom/plugins/ezpublish4/ezpublish4.plugin.zsh
+++ oh-my-zsh-custom/plugins/ezpublish4/ezpublish4.plugin.zsh
@@ -1,5 +1,12 @@
+#
 # eZ Publish 4 aliases.
 #
 
+# bin
 alias ez4cl='php bin/php/ezcache.php --clear-all'
-alias ez4rc='php runcronjobs.php'
+alias ez4checkts='php bin/php/ezchecktranslation.php'
+alias ez4checktpl='php bin/php/eztemplatecheck.php'
+alias ez4al='php bin/php/ezpgenerateautoloads.php'
+
+# Cron
+alias ez4rc='php runcronjobs.php.php'
