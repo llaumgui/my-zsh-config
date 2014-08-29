c7f88c26dd918fa56c7a6ebf15a41dde7aab6b14
Guillaume Kulakowski <guillaume@kulakowski.fr>
2014-08-29 10:01:52 +0200

    Patch function upgrade_oh_my_zsh and remove upgrade_my_zsh

---
+++ oh-my-zsh-custom/functions_patches.zsh
@@ -0,0 +1,25 @@
+#
+# This file is part of My Zsh Config
+#
+# Copyright (c) 2014 Guillaume Kulakowski <guillaume@kulakowski.fr>
+# Depend of Oh My Zsh (https://github.com/robbyrussell/oh-my-zsh)
+#
+# Distributed under the GNU GPL v2. For full terms see the file LICENSE.
+#
+
+
+function upgrade_oh_my_zsh() {
+    # Oh My Zsh upgrade
+    /usr/bin/env ZSH=$ZSH /bin/sh $ZSH/tools/upgrade.sh
+
+    # My Zsh Config upgrade
+    echo "\n\n"
+    printf '\033[0;35m%s\033[0m\n' "Upgrading My Zsh Config"
+    cd "$ZSH_MY_ZSH_CONFIG"
+    if git pull --rebase --stat origin master
+    then
+        printf '\033[0;35m%s\033[0m\n' 'Hooray! My Zsh Config has been updated and/or is at the current version.'
+    else
+        printf '\033[0;31m%s\033[0m\n' 'There was an error updating. Try again later?'
+    fi
+}
