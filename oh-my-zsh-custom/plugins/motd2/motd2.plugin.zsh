285ae8672ebf6a32cd341e3f1faf92449c0af0fe
Guillaume Kulakowski <guillaume@kulakowski.fr>
2014-09-26 17:59:58 +0200

    Init motd2 plugin

---
+++ oh-my-zsh-custom/plugins/motd2/motd2.plugin.zsh
@@ -0,0 +1,11 @@
+#
+# This file is part of My Zsh Config
+#
+# Copyright (c) 2014 Guillaume Kulakowski <guillaume@kulakowski.fr>
+# Depend of Oh My Zsh (https://github.com/robbyrussell/oh-my-zsh)
+#
+# Distributed under the GNU GPL v2. For full terms see the file LICENSE.
+#
+
+# Pseudo zlogin on each login
+clear
