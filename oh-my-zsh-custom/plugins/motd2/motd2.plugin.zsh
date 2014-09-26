46bb0b0f25ab4b23b35b805b0c2f812fe0de1221
Guillaume Kulakowski <guillaume@kulakowski.fr>
2014-09-26 18:04:26 +0200

    Apply motd

--- oh-my-zsh-custom/plugins/motd2/motd2.plugin.zsh
+++ oh-my-zsh-custom/plugins/motd2/motd2.plugin.zsh
@@ -7,5 +7,13 @@
 # Distributed under the GNU GPL v2. For full terms see the file LICENSE.
 #
 
+upinfo ()
+{
+        echo -ne "\t ";uptime | awk /'up/ {print $3,$4,$5,$6,$7,$8,$9,$10}'
+}
+
 # Pseudo zlogin on each login
 clear
+echo -e "${fg[red]}Today is:\t\t${fg[cyan]}" $(date)
+echo -e "${fg[red]}Kernel Information: \t${fg[cyan]}" $(uname -smr)
+echo -e "${fg[red]}Uptime is: \t\t${fg[cyan]}" $(upinfo) "${reset_color}\n"
