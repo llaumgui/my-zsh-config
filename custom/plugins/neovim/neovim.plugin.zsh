5cf2915388398c73ee723a8b62b8618789bd00ab
Guillaume Kulakowski <guillaume@kulakowski.fr>
2021-08-22 21:02:04 +0200

    remove neovim alias and replace by an new plugin

---
+++ custom/plugins/neovim/neovim.plugin.zsh
@@ -0,0 +1,16 @@
+#
+# This file is part of My Zsh Config
+#
+# Copyright (c) 2014-2021 Guillaume Kulakowski <guillaume@kulakowski.fr>
+# Depend of Oh My Zsh (https://github.com/robbyrussell/oh-my-zsh)
+#
+# Distributed under the GNU GPL v2. For full terms see the file LICENSE.
+#
+
+#
+# neovim aliases.
+#
+
+alias vi='nvim'
+alias vim='nvim'
+
