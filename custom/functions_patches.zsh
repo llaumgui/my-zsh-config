2469263319f4c2ef544ae9f31afe138cbf3efac0
Guillaume Kulakowski <guillaume@kulakowski.fr>
2025-03-24 11:58:58 +0100

    📝 Welcome to 2025

--- custom/functions_patches.zsh
+++ custom/functions_patches.zsh
@@ -1,7 +1,7 @@
 #
 # This file is part of My Zsh Config
 #
-# Copyright (c) 2014-2021 Guillaume Kulakowski <guillaume@kulakowski.fr>
+# Copyright (c) 2014-2025 Guillaume Kulakowski <guillaume@kulakowski.fr>
 # Depend of Oh My Zsh (https://github.com/robbyrussell/oh-my-zsh)
 #
 # Distributed under the GNU GPL v2. For full terms see the file LICENSE.
